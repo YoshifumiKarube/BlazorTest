9b13dd46edfdf290a3e8c9eeb51d318676f50917
Yoshifumi karube <karube2525@gmail.com>
2023-11-14 20:52:05 +0900

    スケジュールUIを追加

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,10 +2,17 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
 
+using Syncfusion.Blazor;
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjgxMTQ3OUAzMjMzMmUzMDJlMzBrbWVSOXpHTEFIUUNZS2Z2MzNPVnVBZWVmcnF5eVhSYjdUZGlkL2tpamZRPQ==");
+
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSyncfusionBlazor();
 
 await builder.Build().RunAsync();
